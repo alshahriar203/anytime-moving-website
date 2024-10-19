a0200af8312d05117b41bae8217424f900d17f40
Mahir Al Shahriar <mahir@Mahirs-MacBook-Air.local>
2024-10-19 10:44:14 +0600

    call number

--- src/components/CallButton/index.js
+++ src/components/CallButton/index.js
@@ -8,7 +8,7 @@ function CallButton() {
           window.location.href = 'tel:9029438989';
             }}> 
             <img className={styles.phoneIcon} src={'/assets/black-phone.svg'} alt="phone" />
-            <p className={styles.phoneNumber}>(+880)1730854112</p>
+            <p className={styles.phoneNumber}>9029438989</p>
         </button>
   );
 }
